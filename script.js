af9667cf441261f573f4ee2391fd4050dd5bd8b9
Francesco Trementozzi <76525070+f3ment@users.noreply.github.com>
2021-02-12 17:32:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// typing animation script intro
+var typed = new Typed(".typing", {
+ strings : ['computer engineering student.', 'developer.'],
+ typeSpeed: 50,
+ backspeed: 20,
+ loop: true
+});
+
+// Typing animation passionate to script
+var typed = new Typed(".typing2", {
+  strings : ['computer science.', 'technology.', 'internet.', 'coding.'],
+  typeSpeed: 50,
+  backspeed: 30,
+  loop: true
+ });
