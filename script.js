fd8ba15f0d504ffe8a360f37836d362a56e46599
Francesco Trementozzi <76525070+f3ment@users.noreply.github.com>
2021-02-16 02:30:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// typing animation script intro
+// typing animation 'intro' script 
 var typed = new Typed(".typing", {
- strings : ['computer engineering student.', 'developer.'],
- typeSpeed: 50,
- backspeed: 20,
- loop: true
+ strings : ['^800 computer ^400 engineering ^200 student ^600 .'/*, 'developer.'*/],
+ typeSpeed: 60,
+ /*backspeed: 20,
+ loop: true*/
 });
 
-// Typing animation passionate to script
+// Typing animation 'passionate to' script
 var typed = new Typed(".typing2", {
   strings : ['computer science.', 'technology.', 'internet.', 'coding.'],
   typeSpeed: 50,
   backspeed: 30,
   loop: true
  });
+
+ 
