9bcb33ea53bc928b71aeefb602243c06cfe6bbf7
Francesco Trementozzi <76525070+f3ment@users.noreply.github.com>
2021-03-02 13:03:54 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "f3ment@protonmail.com";
-$subject = "Contact Form WebSite";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $formcontent="From: $name \n Message: $message";
+  $recipient = "f3ment@protonmail.com";
+  $subject = "Contact Form WebSite";
+  $mailheader = "From: $email \r\n";
+  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+  echo "Thank You!";
 ?>
